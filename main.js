5f1f04adf1048a858b07b5aba9640cbb28016841
Tobipeters <oluwatobilobapeterschem@gmail.com>
2020-05-01 23:12:44 +0100

    little fixes

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var number;
 
-addCount =()=>{
-var numbers = document.getElementById('num').value ;
-number = Number(numbers)
-numbers = document.getElementById('num').value =number +=1
-if(numbers > 0){
-    document.getElementById('num').style.color = 'green'
+//For additive counts
+addCount = () => {
+    var numbers = document.getElementById('num').value;
+    number = Number(numbers)
+    numbers = document.getElementById('num').value = number += 1
+    if (numbers > 0) {
+        document.getElementById('num').style.color = 'green'
+    }
 }
-}
-lowerCount =()=>{
-var numbers = document.getElementById('num').value ;
-number = Number(numbers)
-numbers = document.getElementById('num').value =number -=1 
-if (numbers < 0) {
-    document.getElementById('num').style.color = 'red'
-}
+
+//For subtractive counts
+lowerCount = () => {
+    var numbers = document.getElementById('num').value;
+    number = Number(numbers)
+    numbers = document.getElementById('num').value = number -= 1
+    if (numbers < 0) {
+        document.getElementById('num').style.color = 'red'
+    }
 }
